574b7ae51f5e7a940cb4f50eb24d9533639a879d
pdxdave <webdevdave@hotmail.com>
2020-09-25 11:41:32 -0700

    notes index and server

---
+++ index.js
@@ -0,0 +1,10 @@
+// npm run server
+require('dotenv').config()
+
+const server = require('./api/server')
+
+const PORT = process.env.PORT || 5000
+
+server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
